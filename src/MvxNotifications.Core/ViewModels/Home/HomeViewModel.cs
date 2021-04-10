b739a8d03f30b5dfd313ac14a021878e29878f98
Boris Sclauzero <boris.sclauzero@fd2-solutions.it>
2021-04-10 15:08:22 +0200

    adds texts to viewmodel

--- src/MvxNotifications.Core/ViewModels/Home/HomeViewModel.cs
+++ src/MvxNotifications.Core/ViewModels/Home/HomeViewModel.cs
@@ -6,5 +6,29 @@ namespace MvxNotifications.Core.ViewModels.Home
 {
     public class HomeViewModel : BaseViewModel
     {
+        public HomeViewModel()
+        {
+
+        }
+
+
+        #region properties
+
+        private string _title ="Main page";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _welcomeMessage = "Welcome!!\nApp scaffolded with MvxScaffolding\nand Notifications";
+        public string WelcomeMessage
+        {
+            get => _welcomeMessage;
+            set => SetProperty(ref _welcomeMessage, value);
+        }
+
+        #endregion properties
+
     }
 }
